3c1a2622e56ce804e2276a20868d9a161c5c0a80
Eugene Choe <eugene.choe@gmail.com>
2016-10-24 16:11:00 -0700

    Create NavBar component and framework Create Player component and framework Create NavBar/Player css classes

---
+++ public/client/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from 'style';
+
+const NavBar = () => (
+	<div className="nav">
+		<ul>
+			<li><a href="#">Nav Bar >>></a></li>
+			<li><a href="#">RADrad.io</a></li>
+			<li><a href="#">About</a></li>
+			<li><input placeholder="Search..."></input></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</div>
+);
+
+export default NavBar;
