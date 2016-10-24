9a5cf8ba73398c5b28b7f627d033297b348ede3a
Eugene Choe <eugene.choe@gmail.com>
2016-10-24 14:47:10 -0700

    Add intro text Prettified font and bg with CSS

--- public/client/index.js
+++ public/client/index.js
@@ -4,6 +4,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 
-let docBody = "Webpack is doing its thing, with ES2015!";
-document.write(docBody);
 ReactDOM.render(<App />, document.getElementById('app'));
