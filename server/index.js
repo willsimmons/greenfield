cccde185663bca87b55df17d3b39822bd962bbd0
Eugene Choe <eugene.choe@gmail.com>
2016-10-24 14:19:11 -0700

    Complete Webpack configuration for front-end, React now plays with Webpack!

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/../public/client'));
+app.use(express.static(__dirname + '/../public'));
+
+
 
 app.listen(8000, function() {
   console.log('Listening on port 8000');
