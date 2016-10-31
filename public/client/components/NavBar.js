fc3d3949ce28f6390e7078562ee6b5f9936afdf0
Eugene Choe <eugene.choe@gmail.com>
2016-10-31 14:01:39 -0700

    -(auth) comment out navbar state

--- public/client/components/NavBar.js
+++ public/client/components/NavBar.js
@@ -16,10 +16,6 @@ class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-<<<<<<< 6c8405f22d8ce1d67be848b94788f317542cc0b6
-=======
-      authenticated: false
->>>>>>> -(auth) complete user authentication and protected routes
     };
   }
 
@@ -29,18 +25,10 @@ class NavBar extends React.Component {
         <Link to="/" className="logo">RADRADIO</Link>
         <div className="nav">
           <ul className="navList">
-<<<<<<< 6c8405f22d8ce1d67be848b94788f317542cc0b6
             <li><Link to="/recorder" className={"navItem " + (this.props.activeTab === 'recorder' ? 'active' : '')}>Recorder</Link></li>
             <li><Link to="/player" className={"navItem " + (this.props.activeTab === 'player' ? 'active' : '')}>Player</Link></li>
             <li><Link to "/login" className={"navItem " + (this.props.activeTab === 'login' ? 'active' : '')}>Login</Link></li>
             <li><Link to="/register" className={"navItem " + (this.props.activeTab === 'register' ? 'active' : '')}>Register</Link></li>
-=======
-            <li><Link to="/recorder" className="navItem">Recorder</Link></li>
-            <li><Link to="/player" className="navItem">Player</Link></li>
-            <li><a href="#" className="navItem">About</a></li>
-            <li><Link to="/login" className="navItem">Login</Link></li>
-            <li><Link to="/register" className="navItem">Register</Link></li>
->>>>>>> -(auth) complete user authentication and protected routes
             <li onClick={logout}><a href="#" target="_self" className="navItem">Logout</a></li>
           </ul>
         </div>
