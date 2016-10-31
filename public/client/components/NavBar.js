f241d7d1b2f90b1701b45c6a10014aff497d194a
Eugene Choe <eugene.choe@gmail.com>
2016-10-31 15:52:43 -0700

    Cleaning up merge!

--- public/client/components/NavBar.js
+++ public/client/components/NavBar.js
@@ -25,10 +25,10 @@ class NavBar extends React.Component {
         <Link to="/" className="logo">RADRADIO</Link>
         <div className="nav">
           <ul className="navList">
-            <li><Link to="/recorder" className={"navItem " + (this.props.activeTab === 'recorder' ? 'active' : '')}>Recorder</Link></li>
-            <li><Link to="/player" className={"navItem " + (this.props.activeTab === 'player' ? 'active' : '')}>Player</Link></li>
-            <li><Link to "/login" className={"navItem " + (this.props.activeTab === 'login' ? 'active' : '')}>Login</Link></li>
-            <li><Link to="/register" className={"navItem " + (this.props.activeTab === 'register' ? 'active' : '')}>Register</Link></li>
+            <li><Link to="/recorder" className={'navItem ' + (this.props.activeTab === 'recorder' ? 'active' : '')}>Recorder</Link></li>
+            <li><Link to="/player" className={'navItem ' + (this.props.activeTab === 'player' ? 'active' : '')}>Player</Link></li>
+            <li><Link to="/login" className={'navItem ' + (this.props.activeTab === 'login' ? 'active' : '')}>Login</Link></li>
+            <li><Link to="/register" className={'navItem ' + (this.props.activeTab === 'register' ? 'active' : '')}>Register</Link></li>
             <li onClick={logout}><a href="#" target="_self" className="navItem">Logout</a></li>
           </ul>
         </div>
